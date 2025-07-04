683036736cc7359f51d4f9f3f0b2b14d1e26f750
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-04 17:06:51 +0900

    feat: Supabaseセッション取得用のカスタムフックを作成

---
+++ src/app/_hooks/useSupabaseSettion.ts
@@ -0,0 +1,21 @@
+import { supabase } from "@/utils/supabase";
+import { Session } from "@supabase/supabase-js";
+import { useState, useEffect } from "react";
+
+export const useSupabaseSession = () => {
+  const [session, setSession] = useState<Session | null | undefined>(undefined);
+  const [token, setToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetcher = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setSession(session);
+      setToken(session?.access_token || null);
+      setIsLoading(false);
+    };
+    fetcher();
+  }, []);
+  return { session, isLoading, token };
+};
