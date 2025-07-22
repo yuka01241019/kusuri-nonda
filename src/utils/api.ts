cbbc4bcee215bd9e3f3eb60c2b0beff68659308b
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-22 15:39:50 +0900

    refactor: api.ts内のuseSupabaseSession呼び出しを削除しtokenは引数で受け取る方式に変更

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,3 @@
-import { useSupabaseSession } from "@/app/_hooks/useSupabaseSettion";
-
 //HTTPメソッドの型
 type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
 
@@ -7,9 +5,9 @@ type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
 const baseFetch = async <TResponse, TRequest = undefined>(
   method: HttpMethod,
   url: string,
-  body?: TRequest
+  body?: TRequest,
+  token?:string
 ): Promise<TResponse> => {
-  const { token } = useSupabaseSession();
   if (!token) {
     throw new Error("ログイン情報が見つかりません");
   }
@@ -43,10 +41,12 @@ const baseFetch = async <TResponse, TRequest = undefined>(
 
 //共通化したAPI関数
 export const api = {
-  get: <TResponse>(url: string) => baseFetch<TResponse>("GET", url),
-  post: <TResponse, TRequest>(url: string, body: TRequest) =>
-    baseFetch<TResponse, TRequest>("POST", url, body),
-  put: <TResponse, TRequest>(url: string, body: TRequest) =>
-    baseFetch<TResponse, TRequest>("PUT", url, body),
-  delete: <TResponse>(url: string) => baseFetch<TResponse>("DELETE", url),
+  get: <TResponse>(url: string, token: string) =>
+    baseFetch<TResponse>("GET", url, undefined, token),
+  post: <TResponse, TRequest>(url: string, body: TRequest, token: string) =>
+    baseFetch<TResponse, TRequest>("POST", url, body, token),
+  put: <TResponse, TRequest>(url: string, body: TRequest, token: string) =>
+    baseFetch<TResponse, TRequest>("PUT", url, body, token),
+  delete: <TResponse>(url: string, token: string) =>
+    baseFetch<TResponse>("DELETE", url,undefined,token),
 };
