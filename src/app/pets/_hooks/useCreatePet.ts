d3f369e67fc8fdfc4e0becd74147c498a02d8239
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-22 15:10:53 +0900

    refactor: ペット登録APIを共通関数で呼び出すよう修正

--- src/app/pets/_hooks/useCreatePet.ts
+++ src/app/pets/_hooks/useCreatePet.ts
@@ -1,33 +1,10 @@
 import { CreatePetRequest } from "@/app/_types/pet/CreatePet";
-import { useSupabaseSession } from "@/app/_hooks/useSupabaseSettion";
+import { Pet } from "@/app/_types/pet/CreatePet";
+import { api } from "@/utils/api";
 
 export const useCreatePet = () => {
-  const { token } = useSupabaseSession();
   const createPet = async (data: CreatePetRequest) => {
-    try {
-      if (!token) {
-        throw new Error("ログイン情報が見つかりません");
-      }
-      const res = await fetch("/api/pets", {
-        method: "POST",
-        headers: { "Content-Type": "application/json", Authorization: token },
-        body: JSON.stringify(data),
-      });
-      if (!res.ok) {
-        const errorData = await res.json();
-        console.error("APIエラー内容:", errorData); 
-        throw new Error(errorData.message || "登録に失敗しました");
-      }
-      return await res.json();
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error("ペットの登録に失敗しました", error.message);
-        throw new Error(error.message || "エラーが発生しました");
-      } else {
-        console.error("予期せぬエラーが発生しました", error);
-        throw new Error("エラーが発生しました");
-      }
-    }
+    return api.post<Pet, CreatePetRequest>("api/pets", data);
   };
   return { createPet };
 };
