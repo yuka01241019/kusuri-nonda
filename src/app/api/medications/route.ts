4c437298db40b5bb12ad8e3ba8bbad327861dc0e
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-25 16:51:53 +0900

    feat: 薬新規登録APIのユーザー認証・リクエスト受け取り処理を実装

--- src/app/api/medications/route.ts
+++ src/app/api/medications/route.ts
@@ -8,3 +8,27 @@ export type CreateMedicationRequest = {
   form: "TABLET" | "ROUNDTABLET" | "EYEDROP" | "POWDER";
   color: string;
 };
+
+//薬新規登録API
+export const POST = async (request: NextRequest) => {
+  const token = request.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+  if (error || !data?.user) {
+    return NextResponse.json({ status: error?.message }, { status: 400 });
+  }
+  try {
+    const dbUser = await prisma.user.findUnique({
+      where: { supabaseUserId: data.user.id },
+    });
+    if (!dbUser) {
+      return NextResponse.json(
+        { message: "ユーザーが見つかりません" },
+        { status: 400 }
+      );
+    }
+    const body = await request.json();
+    const { name, form, color }: CreateMedicationRequest = body;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
