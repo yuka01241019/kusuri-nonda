f1d6ac358fadf5bf6a030044092725061babdae8
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-28 17:24:26 +0900

    refactor: users APIを一覧取得からログイン中ユーザーの取得に変更

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/app/_lib/prisma";
 import { CreateUserRequest } from "@/app/_types/user/CreateUser";
 import { handleApiError } from "@/utils/handleApiError ";
+import { supabase } from "@/utils/supabase";
 
 //ユーザー新規作成API
 export const POST = async (request: NextRequest) => {
@@ -31,9 +32,25 @@ export const POST = async (request: NextRequest) => {
 
 //登録ユーザー取得API
 export const GET = async (request: NextRequest) => {
+  const token = request.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+  if (error || !data?.user) {
+    return NextResponse.json(
+      { message: error?.message || "認証に失敗しました" },
+      { status: 400 }
+    );
+  }
   try {
-    const users = await prisma.user.findMany();
-    return NextResponse.json({ users }, { status: 200 });
+    const dbUser = await prisma.user.findUnique({
+      where: { supabaseUserId: data.user.id },
+    });
+    if (!dbUser) {
+      return NextResponse.json(
+        { message: "ユーザーが見つかりません" },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json({ user: dbUser }, { status: 200 });
   } catch (error) {
     return handleApiError(error);
   }
