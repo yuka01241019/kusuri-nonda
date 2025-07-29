51c39ec08321e9768d763a17abb5a1eb09aac606
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-29 10:14:15 +0900

    feat: Zodでmedications用バリデーションスキーマと型を定義

---
+++ src/app/medications/_lib/medicationFormSchema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+//薬形状
+export enum Form {
+  TABLET = "TABLET",
+  ROUNDTABLET = "ROUNDTABLET",
+  EYEDROP = "EYEDROP",
+  POWDER = "POWDER",
+}
+//薬の色(TABLET・ROUNDTABLET選択時のみ)
+export enum Color {
+  WHITE = "WHITE",
+  RED = "RED",
+  YELLOW = "YELLOW",
+  GRAY = "GRAY",
+  ORANGE = "ORANGE",
+}
+
+export const medicationFormSchema = z
+  .object({
+    name: z.string().min(1, "薬の名前は必須です"),
+    form: z.nativeEnum(Form).refine((val) => val !== undefined, {
+      message: "薬の形状を選択してください",
+    }),
+    color: z.nativeEnum(Color).optional(),
+  })
+  .refine(
+    (data) =>
+      //TABLET or ROUNDTABLETのときcolorが必須
+      [Form.TABLET, Form.ROUNDTABLET].includes(data.form) ? !!data.color : true,
+    { message: "薬の色を選択してください", path: ["color"] } //colorフィールドにエラーを紐づける
+  );
+
+export type medicationFormData = z.infer<typeof medicationFormSchema>;
