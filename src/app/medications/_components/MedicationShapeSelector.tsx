01780b72dba7a0c74bb26adda7880292df451282
Yuka Kato <tappi_28@yahoo.co.jp>
2025-07-31 12:39:57 +0900

    chore: 薬の形状アイコン用のSVGをインポート

---
+++ src/app/medications/_components/MedicationShapeSelector.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import TabletIcon from "@assets/icons/medication/form/tablet.svg"
+import RoundTabletIcon from "@assets/icons/medication/form/roundtablet.svg"
+import EyedropIcon from "@assets/icons/medication/form/eyedrop.svg"
+import PowderIcon from "@assets/icons/medication/form/powder.svg"
+
