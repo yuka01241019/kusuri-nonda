db1327b2f0dbd4e42c0548c02547704f2e80db8f
Yuka Kato <tappi_28@yahoo.co.jp>
2025-08-03 20:26:20 +0900

    feat: 薬形状選択UIの作成

--- src/app/medications/_components/MedicationShapeSelector.tsx
+++ src/app/medications/_components/MedicationShapeSelector.tsx
@@ -17,12 +17,49 @@ type MedicationShapeSelectorProps = {
 export const MedicationShapeSelector: React.FC<
   MedicationShapeSelectorProps
 > = ({ label, value, onChange, error, disabled }) => {
-//薬形状アイコン一覧(配列にし、mapにて繰り返し表示
+  //薬形状アイコン一覧(配列にし、mapにて繰り返し表示
   const options = [
-    { value: "TABLET", icon: TabletIcon },
-    { value: "ROUNDTABLET", icon: RoundTabletIcon },
-    { value: "EYEDROP", icon: EyedropIcon },
-    { value: "POWDER", icon: PowderIcon },
+    { value: "TABLET", icon: TabletIcon, label: "錠剤" },
+    { value: "ROUNDTABLET", icon: RoundTabletIcon, label: "錠剤(丸)" },
+    { value: "EYEDROP", icon: EyedropIcon, label: "目薬" },
+    { value: "POWDER", icon: PowderIcon, label: "粉薬" },
   ] as const;
-  return <div className=""></div>;
+  return (
+    <div className="text-small">
+      <p className="mb-2">{label}</p>
+      <div className="flex gap-7 justify-center w-full">
+        {options.map(
+          ({ value: optionValue, icon: Icon, label: optionLabel }) => (
+            <label
+              key={optionValue}
+              className="flex flex-col items-center cursor-pointer justify-between"
+            >
+              <Icon
+                className={`w-8 h-8 ${
+                  optionValue === "ROUNDTABLET" ? "translate-y-[4px]" : ""
+                } mb-2`}
+              />
+              <div className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="medicationForm"
+                  value={optionValue}
+                  checked={value === optionValue}
+                  onChange={() =>
+                    onChange(Form[optionValue as keyof typeof Form])
+                  }
+                  disabled={disabled}
+                  className="appearance-none w-4 h-4 border border-textMain rounded-full checked:border-[3.5px]  checked:border-darkPink"
+                />
+                <span>{optionLabel}</span>
+              </div>
+            </label>
+          )
+        )}
+      </div>
+      {error && (
+        <p className="text-red-500 text-xs mt-1 leading-tight">{error}</p>
+      )}
+    </div>
+  );
 };
