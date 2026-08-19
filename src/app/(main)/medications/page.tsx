4806a1fddfa9044568be8c839396e5e4d09987d2
Yuka Kato <tappi_28@yahoo.co.jp>
2026-08-19 18:04:40 +0900

    feat: 取得した薬一覧の状態管理を追加

--- src/app/(main)/medications/page.tsx
+++ src/app/(main)/medications/page.tsx
@@ -1,14 +1,21 @@
 "use client";
-import { GetMedicationsResponse } from "@/app/_types/medication/CreateMedication";
+import {
+  GetMedicationsResponse,
+  Medication,
+} from "@/app/_types/medication/CreateMedication";
 import { api } from "@/utils/api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const MedicationList = () => {
+  //GETした薬一覧の状態を管理
+  const [medications, setMedications] = useState<Medication[]>([]);
+
   useEffect(() => {
     const fetchMedications = async () => {
       const response = await api.get<GetMedicationsResponse>(
         "/api/medications?page=1",
       );
+      setMedications(response.medications);
       console.log(
         response.medications.map((medication) => {
           return medication.name;
