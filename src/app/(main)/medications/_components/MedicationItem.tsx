0be0e728f8ef0ff000bfdb6383152165c4704e13
Yuka Kato <tappi_28@yahoo.co.jp>
2026-09-08 18:59:38 +0900

    style: 薬一覧アイテムの余白と区切り線を調整

--- src/app/(main)/medications/_components/MedicationItem.tsx
+++ src/app/(main)/medications/_components/MedicationItem.tsx
@@ -4,10 +4,18 @@ import { Medication } from "@/app/_types/medication/CreateMedication";
 
 type MedicationItemProps = {
   medication: Medication;
+  isLast: boolean;
 };
 
 export const MedicationItem: React.FC<MedicationItemProps> = ({
   medication,
+  isLast,
 }) => {
-  return <div>{medication.name}</div>;
+  return (
+    <div
+      className={`pb-5 pt-2 ${isLast ? "" : "border-b-[1.25px] border-gray-300 "}`}
+    >
+      {medication.name}
+    </div>
+  );
 };
--- src/app/(main)/medications/_components/MedicationList.tsx
+++ src/app/(main)/medications/_components/MedicationList.tsx
@@ -26,23 +26,30 @@ const MedicationList = () => {
     fetchMedications();
   }, []);
   return (
-  <div className="text-small">
-    <div className="bg-lightPink min-h-screen flex justify-center items-start py-10">
-      <div className="w-full flex justify-center text-textMain">
-        <div className="w-full max-w-[350px] sm:max-w-[390px] md:max-w-[400px] lg:max-w-[420px] bg-white rounded-[16px] px-4 py-6 sm:px-6 sm:py-8 md:px-8 md:py-10 lg:px-8 lg:py-10">
-          <div className="my-[10px]">
-            <h1 className="text-heading1 text-center font-bold mb-[44px]">
-              薬一覧
-            </h1>
-            <div>
-              {medications.map((medication) => {
-                return <MedicationItem key={medication.id} medication={medication} />;
-              })}
+    <div className="text-small">
+      <div className="bg-lightPink min-h-screen flex justify-center items-start py-10">
+        <div className="w-full flex justify-center text-textMain">
+          <div className="w-full max-w-[350px] sm:max-w-[390px] md:max-w-[400px] lg:max-w-[420px] bg-white rounded-[16px] px-4 py-6 sm:px-6 sm:py-8 md:px-8 md:py-10 lg:px-8 lg:py-10">
+            <div className="my-[10px]">
+              <h1 className="text-heading1 text-center font-bold mb-[44px]">
+                薬一覧
+              </h1>
+              <div>
+                {medications.map((medication, index) => {
+                  return (
+                    <MedicationItem
+                      key={medication.id}
+                      medication={medication}
+                      // 最後の薬の場合下線はなし
+                      isLast={index === medications.length - 1}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
