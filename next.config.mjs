c95d326d834facbd7e6108a0dc06864fd25911ad
Yuka Kato <tappi_28@yahoo.co.jp>
2026-09-15 15:42:18 +0900

    fix: SVGのviewBoxを保持するようSVGR設定を修正

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+      rule.test?.test?.(".svg"),
     );
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
@@ -28,8 +28,26 @@ const nextConfig = {
         resourceQuery: {
           not: [...(fileLoaderRule.resourceQuery?.not ?? []), /url/],
         },
-        use: ["@svgr/webpack"],
-      }
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
     );
     // Modify the file loader rule to ignore *.svg, since we have it handled now
     fileLoaderRule.exclude = /\.svg$/i;
