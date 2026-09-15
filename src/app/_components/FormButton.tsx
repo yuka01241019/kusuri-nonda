df4118ac008e1f0f541840d096ecb9db21158fc0
Yuka Kato <tappi_28@yahoo.co.jp>
2026-09-15 19:59:00 +0900

    refactor: FormButtonでonClickとtypeを指定できるように変更

--- src/app/_components/FormButton.tsx
+++ src/app/_components/FormButton.tsx
@@ -6,6 +6,8 @@ type FormButtonProps = {
   icon?: React.ReactNode; //任意のアイコンを渡す
   showIcon?: boolean; //アイコンを表示するかどうか
   variant?: "primary" | "secondary" | "outline";
+  onClick?: () => void;
+  type?: "submit" | "button";
 };
 
 export const FormButton: React.FC<FormButtonProps> = ({
@@ -14,17 +16,20 @@ export const FormButton: React.FC<FormButtonProps> = ({
   icon,
   showIcon = true,
   variant = "primary",
+  onClick,
+  type = "submit",
 }) => {
   const baseClass =
     "w-[200px] h-[44px] font-bold rounded-[8px] flex items-center justify-center gap-1 ";
   const variantClass = {
     primary: "bg-darkPink text-white border border-textMain",
     secondary: "bg-submitBtn text-white border border-transparent",
-    outline:"bg-white text-textMain border-textMain"
+    outline: "bg-white text-textMain border-textMain",
   }[variant];
   return (
     <button
-      type="submit"
+      onClick={onClick}
+      type={type}
       disabled={disabled}
       className={`${baseClass}${variantClass}`}
     >
